d99259f1713df91e3ecd8db58d3e34cefcd2604b
IKEX01 <ccchekingusa@gmail.com>
2025-04-01 17:15:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ class Bot(Client):
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username          
-        print('Bot Started Powered By @TechifyBots')
+        print('Storm initiated Powered By @storm_techh')
 
     async def stop(self, *args):
         await super().stop()
-        print('Bot Stopped Bye')
+        print('Bot Stopped')
 
 Bot().run()
